fffbad568be23ed57caad64f032fc6e5b50c9b5c
ECHO <yangzhouyuan@pangzi.com>
2017-02-28 14:06:03 +0800

    add qdebug()

--- main.cc
+++ main.cc
@@ -1,11 +1,11 @@
-#include "sheetflow_main.h"
+﻿#include "sheetflow_main.h"
 #include <QApplication>
-
+#include <QDebug>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     sheetflow_main w;
     w.show();
-
+    qDebug() << "echo" ;
     return a.exec();
 }
